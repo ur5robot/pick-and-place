8827f5ed9f3ae3e5f49b848c078894d49fe11984
Joe Shepley <ur5robot@gmail.com>
2017-05-11 17:33:06 -0400

    Removing the intermediate testgraspplan directory

--- Object_Manipulation/testgraspplan/pr2_gripper_grasp_planner_cluster/scripts/strip.py
+++
@@ -1,9 +0,0 @@
-from sys import *
-
-f = open('cluster_bounding_box_finder_3d.py', 'r')
-f_out = open('output.py', 'w+')
-for line in f:
-	f_out.write(line.strip(' ')[6:])
-f.close()
-f_out.close()
-print 'Finished.'
